4b2d91414804e69e0635ccb5d978a97b72ae2763
Ciprian Iamandi <ciprian.iamandi@gmail.com>
2022-06-15 23:59:54 +0200

    added commands, events, aggregate, encoder

---
+++ user.core/src/main/java/com/springbank/user/core/events/UserRegisteredEvent.java
@@ -0,0 +1,14 @@
+package com.springbank.user.core.events;
+
+import com.springbank.user.core.models.User;
+import lombok.Builder;
+import lombok.Data;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+@Data
+@Builder
+public class UserRegisteredEvent {
+    @TargetAggregateIdentifier
+    private String id;
+    private User user;
+}
